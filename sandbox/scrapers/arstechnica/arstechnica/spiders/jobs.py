e543e1ca71575cd5f34ff17d7316f6a48e2bf843
Diego Amicabile <diego.amicabile@gmail.com>
2017-10-23 15:50:39 +0200

    Sandbox 2

--- sandbox/scrapers/arstechnica/arstechnica/spiders/jobs.py
+++ sandbox/scrapers/arstechnica/arstechnica/spiders/jobs.py
@@ -1,5 +1,30 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy import Request
+import os
+from time import time
+from time import sleep
+import json
+
+
+from kafka import KafkaProducer
+
+
+def create_message():
+    msg = {
+        "time": int(time() * 1000.0),
+        "msg": "Hello, World!"
+    }
+
+    return msg
+
+    # one broker is enough
+KAFKA_SERVER = os.environ.get("KAFKA_SERVER", "localhost:9092")
+
+producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER,
+                             value_serializer=lambda value: json.dumps(value).encode("utf-8"),
+                             client_id="Simple Producer")
+
 
 
 class JobsSpider(scrapy.Spider):
@@ -11,5 +36,8 @@ class JobsSpider(scrapy.Spider):
 
     def parse(self, response):
         urls = response.xpath('//a[@class="overlay"]/@href').extract()
-        print(urls)
+        for url in urls:
+            producer.send("test", url)
+            sleep(0.5)
+
 
