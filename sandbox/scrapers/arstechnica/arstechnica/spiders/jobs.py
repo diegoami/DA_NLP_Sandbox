451565ec10822b56d3f488bfe302932ecdbbc030
Diego Amicabile <diego.amicabile@gmail.com>
2017-10-23 16:20:36 +0200

    Now never stops

--- sandbox/scrapers/arstechnica/arstechnica/spiders/jobs.py
+++ sandbox/scrapers/arstechnica/arstechnica/spiders/jobs.py
@@ -29,15 +29,37 @@ producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER,
 
 class JobsSpider(scrapy.Spider):
     name = "jobs"
+    V = set()
     allowed_domains = ["arstechnica.com"]
     start_urls = (
         'https://arstechnica.com/','http://arstechnica.com/'
     )
 
     def parse(self, response):
-        urls = response.xpath('//a[@class="overlay"]/@href').extract()
-        for url in urls:
-            producer.send("test", url)
+
+        def process_url(response, url):
+            absolute_url = response.urljoin(url)
+            producer.send("test", absolute_url)
             sleep(0.5)
+
+        url1s = response.xpath('//a[@class="overlay"]/@href').extract()
+        url2s = response.xpath('//h2/a/@href').extract()
+
+        pages = response.xpath('//div[@class="prev-next-links"]/a/@href').extract()
+
+        for url in url1s:
+            process_url(response, url)
+
+        for url in url2s:
+            process_url(response, url)
+
+        for page in pages:
+            absolute_page = response.urljoin(page)
+            if (absolute_page not in self.V):
+                self.V.add(absolute_page)
+                yield Request(absolute_page , callback=self.parse)
+
+
+
 
 
